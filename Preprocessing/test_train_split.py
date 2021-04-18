e30d4c4c95009ecbb8bc7e379e60047b4a158f5b
Mateusz <66701870+Moxiis@users.noreply.github.com>
2021-04-18 18:11:47 +0200

    Renaming annotations
    Adding loop which will rename our annotation files.

--- Preprocessing/test_train_split.py
+++ Preprocessing/test_train_split.py
@@ -15,3 +15,14 @@ for files in os.listdir(images_directory):
 		elif "test" in files:
 			shutil.move(os.path.join(images_directory, files), os.path.join(test_directory, files))
 			shutil.move(os.path.join(images_directory, img), os.path.join(test_directory, img))
+
+# renaming our annotation files so that they are named the same as the corresponding jpg files			
+for file in os.listdir(test_directory):
+  if file.endswith(".txt"):
+    new_file = file.split(".")[0] + ".txt"
+    os.rename(file, new_file)
+	
+for file in os.listdir(train_directory):
+  if file.endswith(".txt"):
+    new_file = file.split(".")[0] + ".txt"
+    os.rename(file, new_file)	
